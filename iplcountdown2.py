4bcf7354d647ec1c6ad2c6d0d256e2cb2e8a22c3
PheasantPresent <91352943+PheasantPresent@users.noreply.github.com>
2021-11-27 15:50:40 -0800

    Add files via upload
    iplcountdown2 = Our countdown timer
    runtext_timer = Our attempt to have iplcountdown2 printed onto adafruit-hat
    samplebase = Where we think we are directing the programs to actual print onto the adafruit-hat

---
+++ iplcountdown2.py
@@ -0,0 +1,28 @@
+
+import datetime, time
+    #def iplcountdown():
+total = 1
+    #while (total > 0):
+     # taking difference in dates
+current_date = datetime.datetime.now()
+till_date = datetime.datetime(2022, 1, 26, 8, 0, 0, 0)
+difference = till_date - current_date
+total = difference.total_seconds()
+
+    # countdown code
+#if (total <= 0):
+        #print("You're going to Disney World!")
+#else:
+        # days
+days = difference.days
+        # hours
+hours, rest = divmod(difference.seconds, 60 * 60)
+        # minutes, seconds
+minutes,seconds_left = divmod(rest, 60)
+def countdown():
+    countdown = ("WOW in " + str(days) + " Days " + str(hours) + " Hours " + str(minutes) + " Mins " + str(seconds_left) + " seconds.")
+
+#return(countdown)
+    #return (countdown)
+
+#iplcountdown()
