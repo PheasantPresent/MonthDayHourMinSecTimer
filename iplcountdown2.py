03720bf87e41129c7237998cba88ab4e23f06ef1
lewij200 <john.k.lewis@disney.com>
2021-12-10 17:41:21 -0500

    Added return statement. Calling from Runtexst.py

--- iplcountdown2.py
+++ iplcountdown2.py
@@ -1,28 +1,29 @@
-
 import datetime, time
-    #def iplcountdown():
+
+# def iplcountdown():
 total = 1
-    #while (total > 0):
-     # taking difference in dates
+# while (total > 0):
+# taking difference in dates
 current_date = datetime.datetime.now()
 till_date = datetime.datetime(2022, 1, 26, 8, 0, 0, 0)
 difference = till_date - current_date
 total = difference.total_seconds()
 
-    # countdown code
-#if (total <= 0):
-        #print("You're going to Disney World!")
-#else:
-        # days
+# countdown code
+# if (total <= 0):
+# print("You're going to Disney World!")
+# else:
+# days
 days = difference.days
-        # hours
+# hours
 hours, rest = divmod(difference.seconds, 60 * 60)
-        # minutes, seconds
-minutes,seconds_left = divmod(rest, 60)
+# minutes, seconds
+minutes, seconds_left = divmod(rest, 60)
+
+
 def countdown():
-    countdown = ("WOW in " + str(days) + " Days " + str(hours) + " Hours " + str(minutes) + " Mins " + str(seconds_left) + " seconds.")
+    countdown = ("WOW in " + str(days) + " Days " + str(hours) + " Hours " + str(minutes) + " Mins " + str(
+        seconds_left) + " seconds.")
+    #Hey this thing goes into runtext.y.
+    return(countdown)
 
-#return(countdown)
-    #return (countdown)
-
-#iplcountdown()
